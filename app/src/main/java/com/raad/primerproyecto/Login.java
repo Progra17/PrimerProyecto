4bcb55ab88c5e8a02d2e82a3fe40b527d4a1ffad
Andres <al05135478@tecmilenio.mx>
2025-10-16 16:12:30 -0600

    Se crea pantalla de formulario con sus elementos graficos. Pendiente de agregar funcionalidad con BD y SQL

--- app/src/main/java/com/raad/primerproyecto/Login.java
+++ app/src/main/java/com/raad/primerproyecto/Login.java
@@ -2,9 +2,9 @@ package com.raad.primerproyecto;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,26 +12,52 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class PrimeraPantalla extends AppCompatActivity {
+public class Login extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_primera_pantalla);
+        setContentView(R.layout.activity_login);
 
-        /*Con el findViewId se tiene acceso al widget desde la vista
-        Button elboton = (Button) findViewById(R.id.button);
-        elboton.setOnClickListener(HizoClicEnElBoton()); //Reacciona al clic, es decir, cuando de un clic va a pasar algo, la funcion es el parentesis
-        //elboton.setText("Nuevo texto");
-*/
-// EN CLASE VOLVER A VER LO DE COMO EXPORTAR LA INFO DE GITHUB Y LO DE APPCOMPATACTIVITY
+        //btnIngresar.setOnClickListener(Ingresar());
+
+        EditText etUsuario = findViewById(R.id.etUsuario);
+        EditText etContra = findViewById(R.id.etContra);
+        Button btnIngresar = findViewById(R.id.btnIngresar);
+        Button btnRegistrar = findViewById(R.id.btnRegistrar);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+
+        btnRegistrar.setOnClickListener(Registrar());
+
+
+
+        /*Con el findViewId se tiene acceso al widget desde la vista
+        Button elboton = (Button) findViewById(R.id.button);
+        elboton.setOnClickListener(HizoClicEnElBoton()); //Reacciona al clic, es decir, cuando de un clic va a pasar algo, la funcion es el parentesis
+        //elboton.setText("Nuevo texto");
+
+
+    }*/
+
+
+
+    /*Metodo para ingresar a la sesion
+    private View.OnClickListener Ingresar() {
+        return v -> {
+
+        };
+    }*/
 /*
     @Override
     protected void onRestart() {
@@ -58,4 +84,13 @@ public class PrimeraPantalla extends AppCompatActivity {
             startActivity(i);
         };
     }*/
+    }
+
+    //Metodo para registrar usuario
+    private View.OnClickListener Registrar() {
+        return v -> {
+            Intent i = new Intent(Login.this, FormularioRegistro.class);
+            startActivity(i);
+        };
+    }
 }
